826d1e29316a868721374dcfe41856d023d87108
Artur Hamernik <hamernik18@gmail.com>
2020-01-26 11:41:17 +0100

    main_functions

--- Lab11/Lab11.cpp
+++ Lab11/Lab11.cpp
@@ -2,16 +2,17 @@
 //
 
 #include <iostream>
+#include <time.h>
 #include "stos.h"
 using namespace std;
-int main()
-{
+void testInt() {
+	srand(time(0));
 	int* tab = new int[10];
-	for (int i = 0; i < 10; i++) tab[i] = i;
+	for (int i = 0; i < 10; i++) tab[i] = (rand() % 100 + 0);
 
 
 	int* tab_2 = new int[5];
-	for (int i = 0; i < 5; i++) tab_2[i] = i;
+	for (int i = 0; i < 5; i++) tab_2[i] = (rand() % 100 + 0);
 
 
 	stos<int> nowy(tab, 10);
@@ -42,6 +43,50 @@ int main()
 	cout << "Nowy dwa został wyczyszczony, jego romiar: " << nowy_dwa.get_size() << endl;
 
 	nowy_dwa.takeoff();
+}
+void testDouble() {
+	srand(time(0));
+	double* tab = new double[10];
+	for (int i = 0; i < 10; i++) tab[i] = (rand()%100 + 0);
+
+
+	double* tab_2 = new double[5];
+	for (int i = 0; i < 5; i++) tab_2[i] = (rand() % 100 + 0);
+
+
+	stos<double> nowy(tab, 10);
+	cout << "Stos Nowy, romiar: " << nowy.get_size() << endl;
+
+	stos<double> nowy_kopia(nowy);
+
+	cout << "Pomyslnie wykonano kopie stosu nowy" << endl;
+
+	stos<double> nowy_dwa(tab_2, 5);
+	cout << "Stos Nowy dwa, romiar: " << nowy_dwa.get_size() << endl;
+
+	for (int i = 0; i < 10; i++) cout << " Zmienna sciagnieta ze stosu Nowy: " << nowy.takeoff() << endl;
+
+	if (nowy.is_empty())
+		cout << "Stos Nowy jest pusty" << endl;
+
+	nowy.put(16.456);
+	cout << "Zmienna " << 16.456 << " dodana na stos Nowy" << endl;
+
+	if (!nowy.is_empty())
+		cout << "Stos Nowy nie jest pusty" << endl;
+
+	nowy_dwa.copy(nowy_kopia);
+	cout << "Nowy dwa pomyslnie rozszerzony o kopie Nowego, jego romiar to teraz " << nowy_dwa.get_size() << endl;
+
+	nowy_dwa.erase();
+	cout << "Nowy dwa zostal wyczyszczony, jego romiar: " << nowy_dwa.get_size() << endl;
+
+	nowy_dwa.takeoff();
+}
+int main()
+{
+	testInt();
+	testDouble();
 
 
 }
